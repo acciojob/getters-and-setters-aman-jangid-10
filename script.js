7b26c132dac00c94dcd94faf66a694ca271c79ff
aman-jangid-10 <125408872+aman-jangid-10@users.noreply.github.com>
2023-04-20 11:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 //complete this code
-class Person {}
+class Person {
+	constructor (name, age){
+		get name(){};
+		set age(){};
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(name() + "is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+
+	function teach(){
+		console.log(name() + 'is teaching');
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
